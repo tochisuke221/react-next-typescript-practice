8f550331276ac14ded3dcc95a3a7defdbf0b705a
tochisuke221 <tochisuke221@gmail.com>
2023-07-10 07:51:42 +0900

    TypeScript超基礎4

--- sayHello.ts
+++ sayHello.ts
@@ -1,11 +1,11 @@
-/** TypeScriptとは */
-// 実行コマンド: 
-//tsc --strictNullChecks sayHello.ts
-// node sayHello.js
+// /** TypeScriptとは */
+// // 実行コマンド: 
+// //tsc --strictNullChecks sayHello.ts
+// // node sayHello.js
 
-const sayHello = (firstName: string) => {
-	console.log('Hello ' + firstName)
-}
+// const sayHello = (firstName: string) => {
+// 	console.log('Hello ' + firstName)
+// }
 
-let firstName: string = "Tochisuke";
-sayHello(firstName)
+// let firstName: string = "Tochisuke";
+// sayHello(firstName)
