4e7d12368da29c1df64e9fe41b6cc76094c66624
tochisuke221 <tochisuke221@gmail.com>
2023-07-10 22:55:31 +0900

    Contextで書き換え

--- react-sample/src/components/ContainerSample.tsx
+++ react-sample/src/components/ContainerSample.tsx
@@ -1,28 +1,69 @@
-import React from 'react'
+// import React from 'react'
 
-// 型を定義
-type ContainerProps = {
-  title: String
-  children: React.ReactNode
+// // 型を定義
+// type ContainerProps = {
+//   title: String
+//   children: React.ReactNode
+// }
+
+// const Container = (props: ContainerProps) => {
+//   const { title, children } = props
+  
+//   return (
+//     <div style={{ background: 'red'}}>
+//       <span>{title}</span>
+//       <div>{children}</div>
+//     </div>
+//   )
+// }
+
+// const Parent = () => {
+//   return (
+//     <Container title="Hello">
+//       <p>ここの部分がchildrenとして渡されて背景色が赤色になる</p>
+//     </Container>
+//   )
+// }
+
+// export default Parent
+
+// 下記にContextで描き直す
+import React from "react";
+
+const TitleContext = React.createContext('')
+
+const Title = () => {
+  return (
+    // Contextのあたいを参照する
+    <TitleContext.Consumer>
+      {
+         title => {
+           return <h1>{title}</h1>
+         }
+      }
+    </TitleContext.Consumer>
+
+  )
 }
 
-const Container = (props: ContainerProps) => {
-  const { title, children } = props
-  
+
+const Header = () => {
   return (
-    <div style={{ background: 'red'}}>
-      <span>{title}</span>
-      <div>{children}</div>
+    <div>
+      <Title></Title>
     </div>
   )
 }
 
-const Parent = () => {
+const Page = () => {
+  const title = 'React Context'
+
   return (
-    <Container title="Hello">
-      <p>ここの部分がchildrenとして渡されて背景色が赤色になる</p>
-    </Container>
+    <TitleContext.Provider value={title}>
+      <Header />
+    </TitleContext.Provider>
+
   )
 }
 
-export default Parent
+export default Page
