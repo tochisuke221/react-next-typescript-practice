a146c1f999df85693df3747ee00486a0e9dadca5
tochisuke221 <tochisuke221@gmail.com>
2023-07-16 15:12:38 +0900

    Controlsタブのpops制御

--- next-sample/src/stories/StyledButton.stories.tsx
+++ next-sample/src/stories/StyledButton.stories.tsx
@@ -1,34 +1,64 @@
-import { Meta } from '@storybook/react'
+// import React, { useState } from 'react'
+// import { Meta, StoryObj } from '@storybook/react'
+// import { StyledButton } from '../components/StyledButton'
+// import { action } from '@storybook/addon-actions'
+// import { type } from 'os'
+
+
+// // ファイル内のStoryの設定
+// export default {
+//   title: 'StyledButton',
+//   component: StyledButton,
+//   argTypes: {
+//     variant: {
+//       control: { type: 'radio' },
+//       options: ['primary', 'success', 'transparent']
+//     },
+//     children: {
+//       control: { type: 'text' },
+//     },
+//     onClick: { action: 'clicked' } 
+//   }
+// } as Meta<typeof StyledButton>
+
+
+// type Template = StoryObj<typeof StyledButton>
+
+// export const Template: Template = {
+//   render: () => <StyledButton/>
+// }
+
+
+import type { Meta, StoryObj } from '@storybook/react';
 import { StyledButton } from '../components/StyledButton'
+// import StyledButton from './StyledButton';
 
-// ファイル内のStoryの設定
-export default {
+const meta = {
   title: 'StyledButton',
-  component: StyledButton
-} as Meta<typeof StyledButton>
+  component: StyledButton,
+  argTypes: {
+     variant: {
+       control: { type: 'radio' },
+       options: ['primary', 'success', 'transparent']
+     },
+     children: {
+       control: { type: 'text' },
+     },
+     onClick: { action: 'clicked' } 
+   }
+} as Meta<typeof StyledButton>;
 
-export const Primary = (props) => {
-  return (
-    <StyledButton {...props} variant="primary" >
-      Primary
-    </StyledButton>
-  )
+export default meta;
+
+type Story = StoryObj<typeof StyledButton>;
+
+export const Button: Story = {
+  render: (props) => {
+    return <StyledButton  {...props}></StyledButton>
+  },
+};
+
+Button.args ={
+  variant: 'transparent',
+  children: 'Transparent'
 }
-
-export const Success = (props) => {
-  return (
-    <StyledButton {...props} variant="success" >
-      Success
-    </StyledButton>
-  )
-}
-
-export const Transparent = (props) => {
-  return (
-    <StyledButton {...props} variant="transparent" >
-      Transparent
-    </StyledButton>
-  )
-}
-
-
